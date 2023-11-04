106510f14e702fe945e569e0c1c514486a86614a
Hunter Adams <hunteradams@Hunters-MBP-2.lan>
2023-11-04 11:31:13 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1914-curtiss-jenny",
+      "name": "1914 Curtiss Jenny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.1107121115984615,
+        "pitch": 0.2873114959835874,
+        "fov": 1.4902605632793264
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.9546564327372913,
+          "pitch": 0.5218535355726921,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards moves the back wing flaps.&nbsp;</div>"
+        },
+        {
+          "yaw": 2.7643981278530623,
+          "pitch": 0.1502313195611471,
+          "title": "Altimeter",
+          "text": "Altitude is defined as height of an object or point in relation to sea level or ground.&nbsp;<div><br></div><div>From the onset, pilots where trying to track their altitude and set records. On August 2, 1908, Selfridge flew the June Bug 75 feets, by 1914 Heinrich Oelerich, a German aircraft designer, set an altitude record of 26, 739 ft.</div>"
+        },
+        {
+          "yaw": -2.833848527413126,
+          "pitch": -0.30783099075650355,
+          "title": "Oil gauge",
+          "text": "The Curtiss Jenny was able to hold about 21 US gallons of fuel. This allowed the plane to travel about 175 miles with a speed of about 80 mph.&nbsp;"
+        },
+        {
+          "yaw": -2.769864646581212,
+          "pitch": 0.03301602866172537,
+          "title": "Victometer",
+          "text": "A victometer is the same thing was a tachometer. It shows the vehicle operator how fast the engine's crankshaft is spinning.&nbsp;<div><br></div><div>This is another measurement for RPM&nbsp; (revolutions per minute) which are used to help determine propulsive power. If the propeller has a fixed pitch, the RPM helps to indicate the power output of the engine. Using the RPM along with the throttle, helps the pilot manage the engine power.</div>"
+        },
+        {
+          "yaw": 3.1079731567061586,
+          "pitch": -0.33769079908760524,
+          "title": "Compass",
+          "text": "Liquid filled compass for navigation.&nbsp;"
+        },
+        {
+          "yaw": 2.577109459840684,
+          "pitch": -0.05766355686193769,
+          "title": "Throttle",
+          "text": "The throttle, also known as a thrust lever, controls the amount of fuel provided to the engine."
+        }
+      ]
+    },
+    {
+      "id": "1-curtiss-robin",
+      "name": "Curtiss Robin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.1083464459201906,
+        "pitch": 0.164422536473964,
+        "fov": 1.4902605632793264
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.0600684944873944,
+          "pitch": -0.15573159319406216,
+          "title": "RPM Tachometer",
+          "text": "In a propeller aircraft the shaft torque and RPM (revolutions per minute) are used to help determine propulsive power. If the propeller has a fixed pitch, the RPM helps to indicate the power output of the engine. Using the RPM along with the throttle, the pilot is able to manage the engine power.&nbsp;<span style=\"color: rgb(0, 0, 0); font-family: Calibri, sans-serif; font-size: 12pt;\">&nbsp;</span><p class=\"MsoNormal\" style=\"margin: 0in; font-size: 12pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400;\"><o:p></o:p></p>"
+        },
+        {
+          "yaw": 3.007959974257494,
+          "pitch": -0.14435794609508967,
+          "title": "Oil Pressure",
+          "text": "<div>Pistons on a aircraft rely on the flow of oil to lubricate to the gears to help prevent wear. If the oil pressure drops below a certain level, the engine will no longer be able to run.&nbsp;<br></div><div><br></div><div>The Curtiss Robin was designed to hold 5 US gallons of oil.</div>"
+        },
+        {
+          "yaw": 2.553850526474422,
+          "pitch": -0.1883294758049434,
+          "title": "Choke",
+          "text": "When starting the plane, the pilot needed to adjust the choke as the oil and coolant warms. The choke allows fuel-rich charge into the cylinder, while reducing the intake of air.&nbsp;"
+        },
+        {
+          "yaw": 2.996442876097415,
+          "pitch": 0.16475112381110257,
+          "title": "Water Temperature Gauge",
+          "text": "This gauge is used to show the temperature for water cooled engines. For take-off, the pilot would need a minimum coolant temperature of 140 degree.&nbsp;"
+        },
+        {
+          "yaw": -2.8088358234040243,
+          "pitch": 0.15417894024384715,
+          "title": "Gasoline",
+          "text": "The Curtiss Robin's tank can hold 50 US gallons of gas (25 gallon tank on each wing).<div><br></div><div>In 1935 the Key Brothers made history in a Curtiss Robin with the longest sustained flight lasting 653 hours and 34 minutes (27 days). During the flight, the Key Brothers were resupplied 432 times by other aircrafts.&nbsp;</div>"
+        },
+        {
+          "yaw": 3.009000844580033,
+          "pitch": -0.39280490006467517,
+          "title": "Airspeed",
+          "text": "The Curtiss Robins top airspeed is 115 mph (184 km/hr) with a cruise speed of 85 mph.&nbsp;"
+        },
+        {
+          "yaw": 3.0236032485929725,
+          "pitch": 0.5396273483966194,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards moves the back wing flaps.&nbsp;</div>"
+        },
+        {
+          "yaw": 2.7828241068148865,
+          "pitch": -0.1299871575947371,
+          "title": "Altitude&nbsp;",
+          "text": "On June 2, 1929 Lt. P.H. Spencer set an altitude record of 17,500 ft in a Curtiss Robin with an X-5 motor."
+        },
+        {
+          "yaw": 2.58623887092042,
+          "pitch": 0.14633373578541686,
+          "title": "On/off Switch",
+          "text": "Curtiss Robin planes had a simple on off swtich."
+        },
+        {
+          "yaw": -2.404993512355368,
+          "pitch": 0.04564762085723473,
+          "title": "Fusealuge&nbsp;",
+          "text": "The Curtiss Robin's fuselage was made of steel tube covered in fabric, while the wings are made of wood and fabric.&nbsp;"
+        },
+        {
+          "yaw": 1.9500494144840097,
+          "pitch": 0.25108239814794686,
+          "title": "Throttle",
+          "text": "The throttle, also known as a thrust lever, controls the amount of fuel provided to the engine."
+        }
+      ]
+    },
+    {
+      "id": "2-1929-mercury-chic-t-2",
+      "name": "1929 Mercury Chic T-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.04308977211997,
+        "pitch": -0.04406811840539504,
+        "fov": 1.4902605632793264
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.909510036222594,
+          "pitch": 0.4241738000271251,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards moves the back wing flaps.&nbsp;</div>"
+        },
+        {
+          "yaw": -2.6167719602756634,
+          "pitch": -0.14178674162047145,
+          "title": "On/off Switch",
+          "text": "In the late 1920s Mercury first built the Chic T-2. It was flown for the first time in 1928. Only 27 planes were built, and out of the 27, only 15 were sold. This was largely due&nbsp; Great Depression. The Curtiss Museum holds one of the only known models for display.&nbsp;&nbsp;"
+        },
+        {
+          "yaw": 2.543123476502122,
+          "pitch": -0.0289721862222585,
+          "title": "Choke",
+          "text": "When starting the plane, the pilot needed to adjust the choke as the oil and coolant warms. The choke allows fuel-rich charge into the cylinder, while reducing the intake of air.&nbsp;"
+        },
+        {
+          "yaw": 2.7828228263751935,
+          "pitch": -0.43634468010025884,
+          "title": "Altitude",
+          "text": "The planes max altitude is 18,000, with a rate climb of 900 ft/min (4.6 m/s)."
+        },
+        {
+          "yaw": -3.0131021371993665,
+          "pitch": -0.4539266265827031,
+          "title": "Tachometer (facing the wrong way, pointing left)",
+          "text": "This plane's max speed was 115 mph with a cruising speed of 95 mph and a landing speed of 40 mph.&nbsp;"
+        },
+        {
+          "yaw": 3.0651497625184376,
+          "pitch": -0.11502221485623032,
+          "title": "Tempature",
+          "text": "For early aviation temperature gauges were used for necessary information such as true airspeed, density of altitude, and for warning of potential icing conditions."
+        },
+        {
+          "yaw": 3.077238703788611,
+          "pitch": -0.4641840819093499,
+          "title": "Oil pressure",
+          "text": "Fuel capacity is 28 US gallons."
+        }
+      ]
+    },
+    {
+      "id": "3-1928-travel-air-2000",
+      "name": "1928 Travel Air “2000”",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0653835255261015,
+        "pitch": 0.27223669939166584,
+        "fov": 1.4902605632793264
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.016046544225482,
+          "pitch": 0.5310948298492786,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards moves the back wing flaps.&nbsp;</div>"
+        },
+        {
+          "yaw": 2.8413780059054563,
+          "pitch": -0.10371894306781115,
+          "title": "Airspeed",
+          "text": "The 1928 Travel Air \"2000\"&nbsp; max airspeed was 100 mph, with a cruise speed of 85 mph, and minimum control speed of 40 mph.&nbsp;&nbsp;<ul style=\"list-style-image: url(&quot;/w/skins/Vector/resources/skins.vector.styles/images/bullet-icon.svg?d4515&quot;); margin-top: 0.3em; margin-left: 1.6em; color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\"><li></li></ul>"
+        },
+        {
+          "yaw": 2.6018031691402594,
+          "pitch": 0.22433983536129887,
+          "title": "On/off Switch",
+          "text": "Travel Air was acquired by the Curtiss-Wright Corporation in 1932. Before that acquisition they had a successful company producing planes. In 1928, Travel Air produced more than 400 planes and became the world's largest manufacturer of commercial monoplanes and biplanes."
+        },
+        {
+          "yaw": 2.5891776943574767,
+          "pitch": -0.1808736283264416,
+          "title": "Fuel Tank",
+          "text": "The fuel capacity for this plane is 42 US gallons, with an oil capicity of 4 US gallons.&nbsp;"
+        },
+        {
+          "yaw": -2.6889201146415864,
+          "pitch": 0.25754050415425667,
+          "title": "Altitude",
+          "text": "&nbsp;On December 7, 1928 Louise Thaden set a new women's altitude record in a Travel Air biplane. Thaden needed fur lined goggles and an oxygen tank for the flight.&nbsp;&nbsp;<div><br></div><div>The Oakland Tribune comment: \" One altimeter showed a height of 25,400 ft, while another registered 23,100 ft. Either mark would be sufficient to break the record of 22,000 ft held by Lady Heath&nbsp; of London.\"&nbsp;&nbsp;</div><p class=\"MsoNormal\" style=\"margin: 0in; font-size: 12pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400;\"><o:p></o:p></p>"
+        },
+        {
+          "yaw": -2.390609318371702,
+          "pitch": -0.04946085117713572,
+          "title": "Water Temperature",
+          "text": "For early aviation temperature gauges were used for necessary information such as true airspeed, density of altitude, and for warning of potential icing conditions."
+        },
+        {
+          "yaw": -2.6580649939273666,
+          "pitch": -0.31876366352680385,
+          "title": "Tachometer",
+          "text": "In a propeller aircraft the shaft torque and RPM (revolutions per minute) are used to help determine propulsive power. If the propeller has a fixed pitch, the RPM helps to indicate the power output of the engine. Using the RPM along with the throttle, helps the pilot manage the engine power.&nbsp;<span style=\"color: rgb(0, 0, 0); font-family: Calibri, sans-serif; font-size: 12pt;\">&nbsp;</span>"
+        },
+        {
+          "yaw": -2.8209058177912567,
+          "pitch": 0.7028598130553547,
+          "title": "Elevator Control",
+          "text": "These foot pedals move the back elevators up or down, thus making the plane go right or left."
+        },
+        {
+          "yaw": -3.012007740135406,
+          "pitch": -0.33487212042851944,
+          "title": "Bank Angle Indicator&nbsp;",
+          "text": "A bank angle indicator tells the pilot the attitude of the plane in the sky relative to the ground. It uses liquid with a bubble that osaltes between the right and left.&nbsp; &nbsp;<div data-hveid=\"CBoQAQ\" style=\"color: rgb(32, 33, 36); font-family: Roboto, arial, sans-serif; font-size: medium; background-color: rgb(255, 255, 255);\"><div jsname=\"tJHJj\" class=\"dnXCYb\" aria-controls=\"_G-E_ZeevJJ-s5NoP6K-LoAs_39\" aria-expanded=\"true\" role=\"button\" tabindex=\"0\" jsaction=\"AWEk5c\" style=\"align-items: center; display: flex; max-height: none; position: relative; width: 652px; cursor: pointer; outline: 0px;\"><div jsname=\"pcRaIe\" class=\"L3Ezfd\" data-ved=\"2ahUKEwinqf3En56CAxUfFlkFHejXArQQuk56BAgaEAI\" style=\"position: absolute; height: 48px; width: 652px; left: 0px; top: 0px; visibility: hidden;\"></div></div></div>"
+        },
+        {
+          "yaw": -2.979572641460786,
+          "pitch": -0.05013445718303444,
+          "title": "Pitch Indicator",
+          "text": "A pitch indicator measures the angle between the horizon and the longitudinal axis of your aircraft.&nbsp;"
+        },
+        {
+          "yaw": 2.3310508210899226,
+          "pitch": 0.42214039894983557,
+          "title": "Throttle&nbsp;",
+          "text": "The throttle, also known as a thrust lever, controls the amount of fuel provided to the engine."
+        },
+        {
+          "yaw": 0.13815526120244215,
+          "pitch": 0.45000181743415446,
+          "title": "Cargo Hold",
+          "text": "This is the access to the cargo hold."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
