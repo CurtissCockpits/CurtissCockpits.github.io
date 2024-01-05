e45dedccaab71c6f81722b28b58ee3c8a848f683
Hunter Adams <hunteradams@Hunters-MBP-2.lan>
2024-01-05 18:34:52 -0500

    test

---
+++ dirigible/data.js
@@ -0,0 +1,111 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-view-of-engine",
+      "name": "Front View of Engine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0729316888676887,
+        "pitch": 0.20661550748716095,
+        "fov": 1.4716910130978276
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.6355930734218163,
+          "pitch": 0.2670220510143011,
+          "title": "The 1904 Engine",
+          "text": "Curtiss modified a two-cylinder, V-type engine for Thomas Baldwin's airship, California Arrow. In 1905, Curtiss developed the twin-cylinder further and renamed it the A-2, which would go on to power many early American dirigibles.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-side-view-of-engine",
+      "name": "Side View of Engine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.1180915922664276,
+        "pitch": 0.3425544139694985,
+        "fov": 1.4716910130978276
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-back-view-of-engine",
+      "name": "Back View of Engine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.06624133347103367,
+        "pitch": 0.20425604808119502,
+        "fov": 1.4716910130978276
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
