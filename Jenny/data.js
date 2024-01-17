10daaebbcc545d2cd54761b1d060216f3dee61ed
Hunter Adams <hunteradams@Hunters-MBP-2.lan>
2024-01-17 09:43:25 -0500

    updated mercury, travel air, robin, and jenny

---
+++ Jenny/data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1914-curtiss-jenny",
+      "name": "1914 Curtiss Jenny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.133477803003929,
+        "pitch": 0.1146713250077891,
+        "fov": 1.4117428265797907
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.120764401876958,
+          "pitch": -0.3738872528286059,
+          "title": "Compass",
+          "text": "Liquid filled compass for navigation."
+        },
+        {
+          "yaw": 2.8987249817959135,
+          "pitch": 0.20270413889626404,
+          "title": "Altimeter",
+          "text": "Altitude is defined as the height of an object or point in relation to sea level or ground.&nbsp;<div><br></div><div>From the onset, pilots were trying to track their altitude in the hopes of setting records. On August 2, 1908, Selfridge flew the June Bug at a height of 75 ft! However, in just a few short years, that record was surpassed by German aircraft designer, Heinrich Oelerich, who flew a whopping 26,739 ft in the air.&nbsp;</div>"
+        },
+        {
+          "yaw": -2.995146687899494,
+          "pitch": 0.5873671821413922,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards, and if moved to the right, it would do the opposite.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards the back wing flap will move right or left.</div>"
+        },
+        {
+          "yaw": -2.723071662401658,
+          "pitch": 0.045366586461989655,
+          "title": "Victometer",
+          "text": "A victometer is the same thing as a tachometer. It shows the vehicle operator how fast the engine's crankshaft is spinning.&nbsp;<div><br></div><div>This is another measurement for RPM (revolutions per minute) which are used to help determine propulsive power. If the propeller has a fixed pitch, the RPM helps to indicate the power output of the engine. Using the RPM along with the throttle, pilots are able to manage the power of the engine.</div>"
+        },
+        {
+          "yaw": -2.8212720534886344,
+          "pitch": -0.276473746769307,
+          "title": "Oil Gauge",
+          "text": "The Curtiss Jenny was able to hold about 21 US gallons of fuel. This allowed the plane to travel about 175 miles with a speed of about 80 mph.&nbsp;"
+        },
+        {
+          "yaw": 2.570270731072828,
+          "pitch": -0.08540565686323731,
+          "title": "Throttle",
+          "text": "The throttle, also known as the thrust lever, controls the amount of fuel provided to the engine.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ Mercury/data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1929-mercury-chic-t-2",
+      "name": "1929 Mercury Chic T-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.9391304708855,
+        "pitch": -0.16342647882679096,
+        "fov": 1.4117428265797907
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.6561367413885435,
+          "pitch": -0.1305932791184219,
+          "title": "On/off Switch&nbsp;",
+          "text": "In the late 1920s Mercury first built the Chic T-2. It was flown for the first time in 1928. Only 27 planes were built, with only 15 being sold. This was largely due to the Great Depression. The Curtiss Museum holds one of the only known models for display.&nbsp;"
+        },
+        {
+          "yaw": -2.9104182692279252,
+          "pitch": 0.33767843887091153,
+          "title": "Control Stick",
+          "text": "Moving the control stick to the left causes the left aileron to move upwards, while simultaneously moving the right aileron downwards, and if moved to the right, it would do the opposite.&nbsp;<div><br></div><div>While moving the control stick forwards and backwards the back wing flap will move right or left.</div>"
+        },
+        {
+          "yaw": -3.109339156083454,
+          "pitch": -0.098114533113133,
+          "title": "Tempature",
+          "text": "Early aviation temperature gauages were used for necessary information such as true speed, density of altitude, and for warning of potential icing conditions.&nbsp;"
+        },
+        {
+          "yaw": 3.103041060751554,
+          "pitch": -0.5006587786623111,
+          "title": "Oil Pressure",
+          "text": "The fuel capacity is 28 US gallons."
+        },
+        {
+          "yaw": 2.7637081293617136,
+          "pitch": -0.4312510344105238,
+          "title": "Altitude",
+          "text": "The planes max altitude is 18,000ft with a rate climb of 900ft/min (4.6 m/s)."
+        },
+        {
+          "yaw": 2.627848993985184,
+          "pitch": -0.029184189577302178,
+          "title": "Choke",
+          "text": "When starting the plane, the pilot needed to adjust the choke as the oil and coolant warms. The choke allows fuel-rich charge into the cylinder, while reducing the intake of the air."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
