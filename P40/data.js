a7f2b152fb4d40f7f8361555d96cd76597c07460
Hunter Adams <hunteradams@Hunters-MBP-2.lan>
2024-05-05 21:17:40 -0400

    p40

---
+++ P40/data.js
@@ -0,0 +1,159 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-p40_1",
+      "name": "P40_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.34497694471479257,
+          "pitch": -0.08858127536218063,
+          "title": "Gunsight/Sun Screen",
+          "text": "Little is known about the horizontal designed gunsight. A reflective image apears on the 90-degree reflective head."
+        },
+        {
+          "yaw": 0.6438958174500815,
+          "pitch": -0.5431170828702179,
+          "title": "Periscope Mirrors (useual location)",
+          "text": "This should be adjusted to give 2 degree drop in the sight line over nose of the airplane. It should also be facing the opposite direction.&nbsp;&nbsp;"
+        },
+        {
+          "yaw": 0.27392986725162594,
+          "pitch": 0.10439586026160441,
+          "title": "Crash Pad (usallu located here)",
+          "text": "Crash pads were installed on the gun sight by two cap screws."
+        },
+        {
+          "yaw": 0.3391795839633147,
+          "pitch": -1.0102921878611362,
+          "title": "Rear View Mirror",
+          "text": "The rear view mirror would have been located outside of the plane.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-p40",
+      "name": "P40",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.06162781510286841,
+          "pitch": -0.30203478208036216,
+          "title": "Altimeter",
+          "text": "Measures altitude&nbsp;"
+        },
+        {
+          "yaw": 0.6252991134134671,
+          "pitch": 0.7701759767193117,
+          "title": "Fuel Gauge",
+          "text": "Reserve wing tank"
+        },
+        {
+          "yaw": -0.44449864760252744,
+          "pitch": -0.2802390897698963,
+          "title": "Flap and Landing Gear Position",
+          "text": "During landing the pilot wants high lift and high drag, so the flaps and slats will be fully deployed.&nbsp;"
+        },
+        {
+          "yaw": -0.721203782588427,
+          "pitch": -0.06285297044928839,
+          "title": "Mixture",
+          "text": "Sets the amount of fuel added to the intake airflow.&nbsp;"
+        },
+        {
+          "yaw": -0.7837040056214377,
+          "pitch": -0.3581659624875009,
+          "title": "Throttle",
+          "text": "Adjusts power to engine."
+        },
+        {
+          "yaw": -0.7192895260129895,
+          "pitch": 0.11748795523139677,
+          "title": "Prop RPM",
+          "text": "This allows operation of propeller and engine at the most efficient RPM and torque for phase of flight.&nbsp; &nbsp;"
+        },
+        {
+          "yaw": -0.992342614681073,
+          "pitch": -0.2826052374415866,
+          "title": "Throttle Quadrant",
+          "text": "The throttle quadrant is the circular segment that houses the thrust levers.&nbsp;&nbsp;"
+        },
+        {
+          "yaw": 1.2410345833524854,
+          "pitch": -0.2757572084449418,
+          "title": "Hand Crank",
+          "text": "The hand crank closes the canpoy."
+        },
+        {
+          "yaw": 1.284374069457261,
+          "pitch": 0.32207819680642835,
+          "title": "Cowl Flaps&nbsp;",
+          "text": "Cowl flaps work in conjunction with cooling fins to regulate the temperature of aircraft engines."
+        },
+        {
+          "yaw": -0.06279826134549005,
+          "pitch": -0.6395561420141789,
+          "title": "Magnetic Compass",
+          "text": "The magnetic compass is the most basic instrument used by pilots to determine or verify aircraft heading.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
